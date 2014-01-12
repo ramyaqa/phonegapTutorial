a492bd5a08fb6d2e5fb39de855f4d5248f0bf2a2
Arvind Palaniswamy <parvind_15@hotmail.com>
2014-01-11 20:50:56 -0800

    second

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ var app = {
             }
         });
     },
+    
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
     initialize: function() {
         this.store = new MemoryStore();
