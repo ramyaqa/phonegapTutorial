70573609985128d0f2a9459fecfb05d797197e02
Arvind Palaniswamy <parvind_15@hotmail.com>
2014-01-12 17:45:41 -0800

    removed Dbg code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 var app = {
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-            var el = new HomeView(self.store).render().el;
-            alert(el.html());
-            $('body').html('test' + el.html());
-            console.log("render complete");
+        this.store = new MemoryStore(function() {            
+            $('body').html(new HomeView(self.store).render().el);
+
         });
     }
 
