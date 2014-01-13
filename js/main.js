8f0f9a48a6a6a9c4e3081a72a00be8e3c2fdf8dd
Arvind Palaniswamy <parvind_15@hotmail.com>
2014-01-12 17:39:13 -0800

    alert test

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var app = {
         var self = this;
         this.store = new MemoryStore(function() {
             var el = new HomeView(self.store).render().el;
+            alert(el.html());
             $('body').html('test' + el.html());
             console.log("render complete");
         });
